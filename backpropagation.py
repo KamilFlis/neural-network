c07b5d7eca60aa53308d995f64603938b1a6b446
Daniel <danel.filipek@gmail.com>
2022-04-24 23:39:10 +0200

    [FEATURE] - Added cumulative method, added plots

--- backpropagation.py
+++ backpropagation.py
@@ -21,52 +21,143 @@ def delta_f(t):
     # return f(t) * (1 - f(t))
 
 
-if __name__ == '__main__':
+def plot_energy_histogram(energy_history, title_prefix):
+    for input_vector in energy_history:
+        plt.plot(energy_history[input_vector], marker='o')
+        plt.title(f"{title_prefix} - X{input_vector}")
+        plt.ylabel('Wartość Energii')
+        plt.xlabel('Liczba przetworzonych wektorów wejściowych')
+        plt.show()
 
-    epochs = 20000
-    learning_rate = 0.5
-    epsilon = 10 ** -12  # ?
-    inputs = np.array([
+
+def process_using_partial_energy_method():
+
+    weights = np.copy(INITIAL_WEIGHTS)
+    print("\n===================== PROCESSING USING PARTIAL ENERGY =====================\n")
+
+    energy_history = dict()
+    for X in INPUTS:
+        energy_history[tuple(X)] = []
+
+    for i in range(ITERATION_LIMIT):
+        print(f"Iteration number : {i}")
+        network_learning_finished_correctly = True
+        for X, d in zip(INPUTS, EXPECTED_OUTPUTS):
+            # feed forward
+            x2_1 = f(X.dot(weights[0]))
+            x2_2 = f(X.dot(weights[1]))
+            output_vector_1 = np.array([1, x2_1, x2_2])
+            x3_1 = f(output_vector_1.dot(weights[2]))
+
+            energy = error(d, x3_1)
+            energy_history[tuple(X)].append(energy)
+            if energy > TOLERATED_ERROR:
+                network_learning_finished_correctly = False
+                print(f"X: {X}, d: {d}, y: {x3_1} - ERROR NOT TOLERATED")
+            else:
+                print(f"X: {X}, d: {d}, y: {x3_1} - ERROR TOLERATED")
+                continue
+
+            # back propagate
+            d3_1 = delta_f(x3_1) * delta_error(d, x3_1)
+            d2_1 = delta_f(x2_1) * weights[2][1] * d3_1
+            d2_2 = delta_f(x2_2) * weights[2][2] * d3_1
+
+            delta_w = np.array([
+                X * LEARNING_RATE * d2_1,
+                X * LEARNING_RATE * d2_2,
+                output_vector_1 * LEARNING_RATE * d3_1
+            ])
+
+            weights = np.add(weights, delta_w)
+
+        if network_learning_finished_correctly:
+            print(f"Network has learned with success!")
+            break
+
+    print("Final Weights : ")
+    print(weights)
+
+    plot_energy_histogram(energy_history, "Metoda energii cząstkowej")
+
+def process_using_cumulative_energy_method():
+    print("\n===================== PROCESSING USING CUMULATIVE ENERGY =====================\n")
+    weights = np.copy(INITIAL_WEIGHTS)
+
+    energy_history = dict()
+    for X in INPUTS:
+        energy_history[tuple(X)] = []
+
+    for i in range(ITERATION_LIMIT):
+        print(f"Iteration number : {i}")
+        network_learning_finished_correctly = True
+        cumulative_delta = np.array([
+            [0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0],
+            [0.0, 0.0, 0.0]
+        ])
+        for X, d in zip(INPUTS, EXPECTED_OUTPUTS):
+            # feed forward
+            x2_1 = f(X.dot(weights[0]))
+            x2_2 = f(X.dot(weights[1]))
+            output_vector_1 = np.array([1, x2_1, x2_2])
+            x3_1 = f(output_vector_1.dot(weights[2]))
+
+            energy = error(d, x3_1)
+            energy_history[tuple(X)].append(energy)
+            if energy > TOLERATED_ERROR:
+                network_learning_finished_correctly = False
+                print(f"X: {X}, d: {d}, y: {x3_1} - ERROR NOT TOLERATED")
+            else:
+                print(f"X: {X}, d: {d}, y: {x3_1} - ERROR TOLERATED")
+                continue
+
+            # back propagate
+            d3_1 = delta_f(x3_1) * delta_error(d, x3_1)
+            d2_1 = delta_f(x2_1) * weights[2][1] * d3_1
+            d2_2 = delta_f(x2_2) * weights[2][2] * d3_1
+
+            delta_w = np.array([
+                X * LEARNING_RATE * d2_1,
+                X * LEARNING_RATE * d2_2,
+                output_vector_1 * LEARNING_RATE * d3_1
+            ])
+
+            cumulative_delta = np.add(cumulative_delta, delta_w)
+
+        if network_learning_finished_correctly:
+            print(f"Network has learned with success!")
+            break
+        else:
+            weights = np.add(weights, cumulative_delta)
+
+    print("Final Weights : ")
+    print(weights)
+
+    plot_energy_histogram(energy_history, "Metoda energii całkowitej")
+
+def main():
+    input("Press any key to start network processing using partial energy method : ")
+    process_using_partial_energy_method()
+    input("Press any key to start network processing using cumulative energy method : ")
+    process_using_cumulative_energy_method()
+
+
+if __name__ == '__main__':
+    ITERATION_LIMIT = 30000
+    LEARNING_RATE = 0.5
+    TOLERATED_ERROR = 10 ** -4 # Epsilon
+    INPUTS = np.array([
         [1, 0, 0],
         [1, 0, 1],
         [1, 1, 0],
         [1, 1, 1]
     ])
-
-    expected_output = np.array([0, 1, 1, 0])
-
-    weight = np.array([
+    EXPECTED_OUTPUTS = np.array([0, 1, 1, 0])
+    INITIAL_WEIGHTS = np.array([
         [0.86, -0.16, 0.28],
         [0.82, -0.51, -0.89],
         [0.04, -0.43, 0.48]
     ])
 
-    for it in range(epochs):
-        energy = 0
-        for X, d in zip(inputs, expected_output):
-            # feed forward
-            x2_1 = f(X.dot(weight[0]))
-            x2_2 = f(X.dot(weight[1]))
-            output_vector_1 = np.array([1, x2_1, x2_2])
-            x3_1 = f(output_vector_1.dot(weight[2]))
-
-            print(f"X: {X}, d: {d}, y: {x3_1}")
-
-            err = error(d, x3_1)
-            # if err < epsilon:
-            #     break
-
-            # back propagate
-            d3_1 = delta_f(x3_1) * delta_error(d, x3_1)
-            d2_1 = delta_f(x2_1) * weight[2][1] * d3_1
-            d2_2 = delta_f(x2_2) * weight[2][2] * d3_1
-
-            delta_w = np.array([
-                X * learning_rate * d2_1,
-                X * learning_rate * d2_2,
-                output_vector_1 * learning_rate * d3_1
-            ])
-
-            weight = np.add(weight, delta_w)
-
-    print(weight)
+    main()
