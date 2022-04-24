25005321a822ae6b01a7683d9f2cd67dc775bda2
Kamil <kam.flis99@gmail.com>
2022-04-24 03:08:51 +0200

    add new dimension kernel to XOR

--- perceptron.py
+++ perceptron.py
@@ -9,7 +9,7 @@ def step_function(x):
 def perceptron(weight, inputs, expected_outputs):
     actual_outputs = np.array([-1, -1, -1, -1])
     xx = np.arange(0, 1.5)
-    for i in range(0, 1000):
+    for i in range(0, 10000):
         for index, (x, d) in enumerate(zip(inputs, expected_outputs)):
             actual_output = step_function(np.dot(weight, x))
             if actual_output != d:
@@ -21,7 +21,7 @@ def perceptron(weight, inputs, expected_outputs):
                     plt.show()
             actual_outputs.put(index, actual_output)
 
-        # print(actual_outputs, weight)
+        print(i, actual_outputs, weight)
         if np.array_equal(actual_outputs, expected_outputs):
             print("Perceptron finished with weight: ", weight)
             break
@@ -30,8 +30,8 @@ def perceptron(weight, inputs, expected_outputs):
 def bupa(weight, inputs, expected_outputs):
     actual_outputs = np.array([-1, -1, -1, -1])
     xx = np.arange(0, 1.5)
-    for i in range(0, 1000):
-        z = np.array([0, 0, 0])
+    for i in range(0, 50000):
+        z = np.zeros(len(weight))
         for index, (x, d) in enumerate(zip(inputs, expected_outputs)):
             actual_output = step_function(np.dot(weight, x))
             actual_outputs.put(index, actual_output)
@@ -40,14 +40,14 @@ def bupa(weight, inputs, expected_outputs):
             if d != y:
                 z = np.add((d - y) * x, z)
         weight = np.add(weight, z)
-        if weight[2] != 0:
-            yy = - weight[0] / weight[2] - xx * weight[1] / weight[2]
-            plt.scatter([0, 0, 1, 1], [0, 1, 0, 1])
+        # if weight[2] != 0:
+        #     yy = - weight[0] / weight[2] - xx * weight[1] / weight[2]
+            # plt.scatter([0, 0, 1, 1], [0, 1, 0, 1])
             # plt.scatter([0, 0, 1, 1], [1, 0.6, 0.6, 1])
-            plt.plot(xx, yy)
-            plt.show()
+            # plt.plot(xx, yy)
+            # plt.show()
 
-        # print(actual_outputs, weight)
+        print(i, actual_outputs, weight)
         if np.array_equal(actual_outputs, expected_outputs):
             print("BUPA finished with weight: ", weight)
             break
@@ -65,7 +65,8 @@ def rbf(vector):
 if __name__ == '__main__':
 
     # AND
-    weight = np.array([0.5, 0, 1])
+    # weight = np.array([0.5, 0, 1])
+    weight = np.array([0, 0, 0])
     inputs = np.array([
         [1, 0, 0],
         [1, 0, 1],
@@ -78,12 +79,12 @@ if __name__ == '__main__':
     bupa(weight, inputs, expected_outputs)
 
     # XOR
-    weight = np.array([0.5, 0, 1])
+    weight = np.array([0, 0, 0, 0])
     inputs = np.array([
-        [0, 0, kernel(0, 0)],
-        [0, 1, kernel(0, 1)],
-        [1, 0, kernel(1, 0)],
-        [1, 1, kernel(1, 1)]
+        [1, 0, 0, kernel(0, 0)],
+        [1, 0, 1, kernel(0, 1)],
+        [1, 1, 0, kernel(1, 0)],
+        [1, 1, 1, kernel(1, 1)]
     ])
     expected_outputs = np.array([0, 1, 1, 0])
 
